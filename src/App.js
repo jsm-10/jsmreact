f6e7c335abe0e7249708508e5bc67ec83e756587
Juan Sebastian <jsm.juansebas@gmail.com>
2022-06-10 22:09:39 -0300

    prueba

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import logo from './assets/logosfondo.png'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="logo nuevo" alt="logo" />
+        <p>
+          <h6>Sitio en Construccion</h6>
+        </p>
+        <p>Proximamente</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Bhance.js
@@ -0,0 +1,14 @@
+import './Bhance.css';
+function Bhance(){
+return(
+    <a
+    className="asd"
+    href="https://behance.net/MJSolutions"
+    target="_blank"
+    rel="noopener noreferrer"
+>
+    Mira lo que hacemos
+</a>)
+}
+export default Bhance;
+
